2f04e584fb14ecc8882c1e1bff6385a7e73daba5
maffettone <phillip.maffettone@gmail.com>
2020-08-24 03:20:54 -0400

    Update history and entropy scaling

--- bad_seeds/simple/bad_seeds_02.py
+++ bad_seeds/simple/bad_seeds_02.py
@@ -6,7 +6,7 @@ from tensorforce.environments import Environment
 
 
 class BadSeeds02(Environment):
-    def __init__(self, seed_count, bad_seed_count, history_block=0, reward_ratio=0.1,
+    def __init__(self, seed_count, bad_seed_count, *, history_block=0, reward_ratio=0.1, entropy_scaling=0.,
                  bad_mean=0., bad_var=10., good_mean=0., good_var=1.):
         """
         Bad Seeds 2: Electric Boogaloo
@@ -33,6 +33,10 @@ class BadSeeds02(Environment):
             Memory of trials to ignore
         reward_ratio: float
             [0, 1), ratio of score for pulling a good seed to pulling a bad seed
+        entropy_scaling: float
+            Scale factor to multiple entorpy increase reward drive. A higher value will drive a more uniform sampling.
+            At present this is very experimental and requires further investigation to understand acceptable ranges.
+            It is currently recommended to use a larger history block to avoid redundancy
         bad_mean: float
         bad_var: float
         good_mean: float
@@ -53,6 +57,7 @@ class BadSeeds02(Environment):
         self.bad_var = bad_var
         self.good_mean = good_mean
         self.good_var = good_var
+        self.entropy_scaling = entropy_scaling
 
         self.rng = np.random.default_rng()
 
@@ -63,13 +68,13 @@ class BadSeeds02(Environment):
         self.bad_seed_indices = None
         self.good_seed_indices = None
         self.state = None
-        self.state_shape = (self.seed_count, 3)
+        self.state_shape = (self.seed_count, 4)
         # self.reset()
         self.full_exp = [[] for _ in range(seed_count)]
 
     def states(self):
         """
-        State is [[mean, std, count]]
+        State is [[mean, std, count, in_history]]
         Returns
         -------
         state specification
@@ -123,7 +128,7 @@ class BadSeeds02(Environment):
         self.state = np.zeros(self.state_shape)
         for i, seed in enumerate(self.all_seeds):
             measurements = seed(size=2)
-            self.state[i, :] = [np.mean(measurements), np.var(measurements), 2]
+            self.state[i, :] = [np.mean(measurements), np.var(measurements), 2, 0]
             self.full_exp[i].extend(measurements)
 
         return self.state
@@ -135,6 +140,28 @@ class BadSeeds02(Environment):
     @staticmethod
     def update_var(prev_var, prev_mean, prev_count, new_measurement):
         return prev_count / (prev_count + 1) * (prev_var + (new_measurement - prev_mean) ** 2 / (prev_count + 1))
+
+    @staticmethod
+    def relative_entropy(arr):
+        """
+        Calculates the entropy of a discrete probability distribution relative to a uniform dist of the same len
+        Bounded between [0, 1]
+        Parameters
+        ----------
+        arr: arraylike
+            Discrete probability mass function, or counts
+
+        Returns
+        -------
+        rel_entropy: float
+        """
+        pmf = np.array(arr)
+        pmf = pmf / np.sum(pmf)
+        entropy = 0
+        for p in pmf:
+            if p > np.finfo(np.float32).eps:
+                entropy += p * np.log(p)
+        return entropy/np.log(1/len(pmf))
 
     def execute(self, actions):
         """
@@ -154,11 +181,14 @@ class BadSeeds02(Environment):
         reward: float
         """
         self.timestep += 1
-        mean_index, var_index, count_index = 0, 1, 2
+        mean_index, var_index, count_index, history_index = 0, 1, 2, 3
         seed_index = actions
         seed_measurement = self.all_seeds[seed_index]()
         prev_count = self.state[seed_index, count_index]
         prev_mean = self.state[seed_index, mean_index]
+        if self.entropy_scaling:
+            prev_entropy = self.relative_entropy(self.state[:, count_index])
+
         self.state[seed_index, mean_index] = self.update_mean(prev_mean=prev_mean,
                                                               prev_count=prev_count,
                                                               new_measurement=seed_measurement)
@@ -183,13 +213,26 @@ class BadSeeds02(Environment):
             reward = self.good_seed_reward
         else:
             raise NotImplementedError("Seed index neither in good nor bad seed list.")
+        if self.entropy_scaling:
+            current_entropy = self.relative_entropy(self.state[:, count_index])
+            # One option to increase reward for increasing entropy by factor of entropy_scaling
+            # entropy_reward = self.entropy_scaling * (current_entropy - prev_entropy > 0)
+            # An alternative to the bool reward of increased entropy is to scale the percent change
+            entropy_reward = self.entropy_scaling * (current_entropy - prev_entropy)/prev_entropy
+            print(entropy_reward)
+            reward += entropy_reward
 
+        # Update history queue and update state
         self.history.append(seed_index)
+        self.state[:, history_index] = 0
+        for i in self.history:
+            self.state[i, history_index] = 1
         return self.state, terminal, reward
 
 
 if __name__ == "__main__":
-    env = Environment.create(environment=BadSeeds02, seed_count=10, bad_seed_count=3, history_block=1,
+    np.set_printoptions(precision=3)
+    env = Environment.create(environment=BadSeeds02, seed_count=10, bad_seed_count=3, history_block=1, reward_ratio=0.,
                              max_episode_timesteps=3)
     env.reset()
     print(env.state)
