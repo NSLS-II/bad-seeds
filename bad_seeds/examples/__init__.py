f1135cd22aeba641548b507738efafb72e68b1f4
maffettone <phillip.maffettone@gmail.com>
2021-02-03 22:26:29 -0500

    Add examples module

---
+++ bad_seeds/examples/__init__.py
@@ -0,0 +1,12 @@
+# TODO: Add runtime examples that were used to generate the results present in the paper.
+"""
+These can be shorter training cycles than used in the actual paper, with comments for what is appropriate for serious work.
+Ideally we have something the user can install, and run within 10 minutes on a laptop to get a feel, with the comments
+or separate function args/kwargs for the tough stuff.
+
+# Something like this?
+from training.train_a2c_cartseed import main
+toy_kwargs = [{}....{}]
+for kwargs in toy_kwargs:
+    main(**kwargs)
+"""
