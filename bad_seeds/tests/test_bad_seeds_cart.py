f1fe67ac9829b0616023732e42f75fef53c67d0e
maffettone <phillip.maffettone@gmail.com>
2021-02-03 20:59:11 -0500

    Testing list of demands

---
+++ bad_seeds/tests/test_bad_seeds_cart.py
@@ -0,0 +1,14 @@
+# Initialization
+# Reset
+# bad initialization
+# play
+# sequential
+# frozen order
+# revisiting on
+# revisiting off
+# too many bad seeds
+# variable number bad seeds
+# terminal exit
+# Not callable as reward
+# Multi functional max_episode_timesteps (with and without kwarg)
+# Varying levels of max_episode_timesteps
