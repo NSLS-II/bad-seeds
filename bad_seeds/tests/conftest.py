34647f4a49170248dca3cf57a48ab9027cb1d80c
maffettone <phillip.maffettone@gmail.com>
2021-02-05 12:47:50 -0500

    default environement adjustments

--- bad_seeds/tests/conftest.py
+++ bad_seeds/tests/conftest.py
@@ -1,10 +1,30 @@
 import pytest
-
+from bad_seeds.environments.cartseed import CartSeedCountdown, CartSeed
+from tensorforce.environments import Environment
 
 @pytest.fixture
-def default_environment():
-    from bad_seeds.environments.cartseed import CartSeedCountdown
-    from tensorforce.environments import Environment
+def default_cartseed():
+    """
+    This environment should lock into place the basics, with 10 seeds, each requiring
+    Returns
+    -------
+    Environment
+    """
+    env = CartSeed(seed_count=10,
+                   bad_seed_count=3,
+                   frozen_order=True,
+                   )
+    env = Environment.create(environment=env)
+    return env
+
+@pytest.fixture
+def default_cartseedcountdown():
+    """
+    This environment should lock into place the basics, with 10 seeds, each requiring
+    Returns
+    -------
+    Environment
+    """
     env = CartSeedCountdown(seed_count=10,
                             bad_seed_count=3,
                             frozen_order=True,
